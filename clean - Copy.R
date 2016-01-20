06f3cc0ef15de43e219b347acb4780003a872416
jenni <jennik3379@gmail.com>
2016-01-20 15:02:46 -0500

    changes

---
+++ clean - Copy.R
@@ -0,0 +1,5 @@
+activity$day <- weekdays(as.Date(activity$date))
+activity$DateTime<- as.POSIXct(activity$date, format="%Y-%m-%d")
+
+##pulling data without nas
+clean <- activity[!is.na(activity$steps),]
